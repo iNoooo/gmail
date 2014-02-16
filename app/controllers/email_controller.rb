7fd25506a44c37363f800ec79d0d2ea856d21d29
iNoo <jyy51good@gmail.com>
2014-02-17 01:57:41 +0900

    이메일 전송 기능 실재 탑재

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,19 @@ class EmailController < ApplicationController
   end
 
   def email_send
-    @email = params[:email]
-    @content = params[:content]
+    email = params[:email]
+    title = params[:title]
+    content = params[:content]
+
+    gmail = Gmail.connect("jyy51good@gmail.com", "1gozldsh")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+    end
+
+    gmail.logout
   end
 end
